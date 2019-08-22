213e5b2e34e1f57426ed464f677f6a56cafcd37a
Jeff Trull <jeff.trull@ciere.com>
2019-08-22 05:47:56 -0700

    More refactoring and generalizing

--- acc_vs_partsum.cpp
+++ acc_vs_partsum.cpp
@@ -1,6 +1,8 @@
 // Compare std::accumulate vs std::partial_sum to measure raw read vs. write latency
 // Copyright 2019 Jeff Trull <edaskel@att.net>
 
+#include "benchmark_scan.hpp"
+
 #include <iostream>
 #include <vector>
 #include <numeric>
@@ -9,58 +11,28 @@
 // introduce Google Benchmark
 #include <benchmark/benchmark.h>
 
-// a fixture that sets up the input for us
-// I started with the gbench fixture one but I can't see how to register
-// a nice name for it - always comes out FIXTURENAME/BENCHNAME
-struct RandomInputFixture
-{
-    RandomInputFixture(benchmark::State const & state)
-    {
-        // create and fill random vector of desired size
-        std::random_device rnd_device;
-        std::mt19937 mersenne_engine{rnd_device()};
-        std::uniform_int_distribution<int> dist{1, 20};
-
-        // get input size from first range argument (supplied below)
-        auto sz = state.range(0);
-        data.resize(sz);
-        std::generate(data.begin(), data.end(),
-                      [&]() { return dist(mersenne_engine); });
-    }
-
-    std::vector<int> data;
-};
-
 int main(int argc, char* argv[])
 {
     // process and remove gbench arguments
     benchmark::Initialize(&argc, argv);
 
-    // register programmatically so we can have nice labels
-    // i.e. "std::accumulate" instead of "RandomInputFixture/Accumulate"
-
     benchmark::RegisterBenchmark(
         "std::accumulate",
         [](benchmark::State &state)
         {
-            RandomInputFixture inp(state);
+            RandomInputFixture<int> inp(state);
             for (auto _ : state) {
                 int result = std::accumulate(inp.data.begin(), inp.data.end(), 0);
                 benchmark::DoNotOptimize(result);
             }
-        })->Range(10, 40000000)->UseRealTime();
+        })->Range(8, 1 << 25)->UseRealTime();
 
-    benchmark::RegisterBenchmark(
+    register_benchmark(
         "std::partial_sum",
-        [](benchmark::State &state)
-        {
-            RandomInputFixture inp(state);
-            std::vector<int> result = inp.data;
-            for (auto _ : state) {
-                std::partial_sum(result.begin(), result.end(), result.begin());
-                benchmark::DoNotOptimize(result);
-            }
-        })->Range(10, 40000000)->UseRealTime();
+        std::partial_sum<
+            std::vector<int>::const_iterator,
+            std::vector<int>::iterator>);
+
 
     benchmark::RunSpecifiedBenchmarks();
 }
---
+++ benchmark_scan.hpp
@@ -0,0 +1,64 @@
+// Benchmarking code for inclusive_scan
+// Copyright 2019 Jeff Trull <edaskel@att.net>
+
+#ifndef BENCHMARK_SCAN_HPP
+#define BENCHMARK_SCAN_HPP
+
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <numeric>
+#include <random>
+#include <string>
+
+#include <benchmark/benchmark.h>
+
+//
+// break out all the code necessary to benchmark an inclusive_scan implementation here
+//
+
+// a fixture that sets up the input for us
+// I started with the gbench fixture one but I can't see how to register
+// a nice name for it - always comes out FIXTURENAME/BENCHNAME
+template<typename T>
+struct RandomInputFixture
+{
+    RandomInputFixture(benchmark::State const & state)
+    {
+        // create and fill random vector of desired size
+        std::random_device rnd_device;
+        std::mt19937 mersenne_engine{rnd_device()};
+        std::uniform_int_distribution<T> dist{1, 20};
+
+        // get input size from first range argument (supplied below)
+        auto sz = state.range(0);
+        data.resize(sz);
+        std::generate(data.begin(), data.end(),
+                      [&]() { return dist(mersenne_engine); });
+    }
+
+    std::vector<T> data;
+};
+
+// create and register benchmark on a scan function
+template <typename InputIt, typename OutputIt>
+void
+register_benchmark(const char * name,
+                   OutputIt (*fn)(InputIt, InputIt, OutputIt))
+{
+    using T = typename std::iterator_traits<InputIt>::value_type;
+
+    benchmark::RegisterBenchmark(
+        name,
+        [fn](benchmark::State & state)
+        {
+            RandomInputFixture<T> inp(state);
+            std::vector<T> result = inp.data;
+            for (auto _ : state) {
+                fn(result.begin(), result.end(), result.begin());
+                benchmark::DoNotOptimize(result);
+            }
+        })->Range(8, 1 << 25)->UseRealTime();
+}
+
+#endif // BENCHMARK_SCAN_HPP
